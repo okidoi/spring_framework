d799fb77a6f0ca873a5f1a5a4b537e14ae5f6359
okidoi <okidoi@gmail.com>
2020-02-24 21:00:09 -0300

    Novo commit

--- SpringFramework/src/br/okidoi/projspring/beans/BeanPessoa.java
+++ SpringFramework/src/br/okidoi/projspring/beans/BeanPessoa.java
@@ -3,6 +3,15 @@ package br.okidoi.projspring.beans;
 public class BeanPessoa {
 	
 	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 	
 
 }
